77f6ca65b2ecc21e2d513b4d6c0726f2f2149cae
MamunVerse <mamunurrashid6424@gmail.com>
2022-09-19 06:04:33 +0600

    change post dispatch login from component and add reducer & prepare  function into postAdded object

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,28 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id : 1,
-        title : 'This is first title',
-        content : 'This is first content'
-    },
-    {
-        id : 2,
-        title : 'This is Second title',
-        content : 'This is Second content'
-    }
-]
+  {
+    id: 1,
+    title: "This is first title",
+    content: "This is first content",
+  },
+  {
+    id: 2,
+    title: "This is Second title",
+    content: "This is Second content",
+  },
+];
 
 const postsSlice = createSlice({
-    name : 'posts',
-    initialState,
-    reducers : {
-        postAdded(state, action){
-            state.push(action.payload);
-        }
-    }
-}) 
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
+    },
+  },
+});
 
 export const selectAllPosts = (state) => state.posts;
-export const {postAdded} = postsSlice.actions;
+export const { postAdded } = postsSlice.actions;
 export default postsSlice.reducer;
