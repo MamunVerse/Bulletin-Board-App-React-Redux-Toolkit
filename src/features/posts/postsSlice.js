94eb75e4e6964c86bf0840082aa86a5415f22f1f
MamunVerse <mamunurrashid6424@gmail.com>
2022-09-21 03:04:57 +0600

    Add reaction button on every post and it working 100%

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -70,5 +70,5 @@ const postsSlice = createSlice({
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
